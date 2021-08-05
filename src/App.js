dbab512e0f4d2a5f4c161ed0b4fae366eacd919d
Thomas Sankara <thomassankara74@gmail.com>
2021-08-05 15:38:58 +0300

    set up react-router-dom + render pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Header from "./components/Header"
+import Homepage from "./pages/Homepage"
+import Blog from "./pages/Blog"
+import SinglePost from "./pages/SinglePost"
+import Error from "./pages/Error"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <Homepage />
+        </Route>
+        <Route path="/blog/:slug" children={<SinglePost />}></Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
